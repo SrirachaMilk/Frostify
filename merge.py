7a65930194a9481f82f601c4c0dd896823301127
Grayson Arthur <garthur@alarm.com>
2022-10-27 14:15:35 -0400

    Remove some comments

--- merge.py
+++ merge.py
@@ -24,23 +24,6 @@ def merge_edit_gcodes():
                         finalLine = finalLine + token + " "
                 data += finalLine
 
-    """     with open("part_two.gcode") as fp:
-        for line in fp:
-            if line.strip() == ';LAYER:0':  # Or whatever test is needed
-              break
-    # Reads text until the end of the block:
-        for line in fp:  # This keeps reading the file
-            if line.strip() == ';End of Gcode':
-              break
-            finalLine = ""
-            lineTokens = line.split()
-            for token in lineTokens:
-                if token.contains('Z'):
-                    continue
-                else:
-                    finalLine = finalLine + token + " "
-            data2+=line
-     """
     # Merging 2 files to next line
     data += "\n"
     #data += data2

b9869a754987b6f1563861ae2591b0ec282906c2
Nafew <nafewnowsh@gmail.com>
2022-10-27 13:46:47 -0400

    remove heating parts

--- merge.py
+++ merge.py
@@ -1,13 +1,16 @@
 def main():
     merge_gcodes()
 
-def merge_gcodes():  
+def merge_gcodes(): 
+    bad_words = ['heating'] 
     data = ""
     data2 = ""
     with open("part_one.gcode") as fp:
-        data = fp.read()
+        for line in fp:
+            if not any(bad_word in line for bad_word in bad_words):
+                data += line
 
-    with open("part_two.gcode") as fp:
+    """     with open("part_two.gcode") as fp:
         for line in fp:
             if line.strip() == ';LAYER:0':  # Or whatever test is needed
               break
@@ -16,10 +19,10 @@ def merge_gcodes():
             if line.strip() == ';End of Gcode':
               break
             data2+=line
-    
+     """
     # Merging 2 files to next line
     data += "\n"
-    data += data2
+    #data += data2
     print(data)
     with open ("full_design.gcode", 'w') as fp:
         fp.write(data)
